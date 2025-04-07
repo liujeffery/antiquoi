f433ae6b2daf96eafd66af4439af6df472725009
Edward <edstudies12@gmail.com>
2025-04-07 10:29:58 -0400

    created text agent knowledge source class

--- backend/text_agent.py
+++ backend/text_agent.py
@@ -4,21 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def analyze_description(description: str) -> str:
-    dps_key = os.getenv('DPS_API')
-    client = OpenAI(
-        base_url="https://openrouter.ai/api/v1",
-        api_key=dps_key,
-    )
+class TextAgent(KnowledgeSource):
+    def __init__(self):
+        self.opinion = ""
 
-    prompt = ("Given the following description of an item, please give a response about what the item is, "
-              "a description of the item, and a price range. Please give your answer in the following form: "
-              "Item: <item here> \n Description: <description here> \n Max price: <max price> \n Min price <min price> \n Condition: <condtion of either new, used like new, used good, or used fair>. "
-              + description)
+    def get_expert_opinion(self):
+        return self.opinion
 
-    response = client.chat.completions.create(
-        model="deepseek/deepseek-r1:free",
-        messages=[{"role": "user", "content": prompt}]
-    )
+    def analyze_description(description: str) -> str:
+        dps_key = os.getenv('DPS_API')
+        client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=dps_key,
+        )
 
-    return response.choices[0].message.content
+        prompt = ("Given the following description of an item, please give a response about what the item is, "
+                  "a description of the item, and a price range. Please give your answer in the following form: "
+                  "Item: <item here> \n Description: <description here> \n Max price: <max price> \n Min price <min price> \n Condition: <condtion of either new, used like new, used good, or used fair>. "
+                  + description)
+
+        response = client.chat.completions.create(
+            model="deepseek/deepseek-r1:free",
+            messages=[{"role": "user", "content": prompt}]
+        )
+
+        return response.choices[0].message.content
